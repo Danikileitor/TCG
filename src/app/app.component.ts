abe862e919927328e3854ee7cecc55c510d690e3
Danikileitor <focusyi@hotmail.com>
2025-06-12 16:42:19 +0200

    random

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ScryfallComponent } from "./scryfall/scryfall.component";
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ScryfallComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ScryfallComponent } from './scryfall/scryfall.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: 'scryfall', component: ScryfallComponent
+}];
