609614bf37bc4355ccbc22059b1cb41afa04d0f0
Danikileitor <focusyi@hotmail.com>
2025-06-12 18:29:52 +0200

    random

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,12 @@
 import { Routes } from '@angular/router';
 import { ScryfallComponent } from './scryfall/scryfall.component';
 import { ScryfallRandomComponent } from './scryfall/random/random.component';
-import { AppComponent } from './app.component';
 
 const baseTitle = "[DNK]{TCG}"
 
 export const routes: Routes = [
     {
-        path: 'scryfall', title: baseTitle + "[MTG]", component: ScryfallComponent, children: [
+        path: 'MTG', title: baseTitle + "[MTG]", component: ScryfallComponent, children: [
             {
                 path: 'random', title: baseTitle + "[MTG - Random]", component: ScryfallRandomComponent
             }
