c49d58d038603deab29aabc0a1bc8fdf7a296c6b
Danikileitor <focusyi@hotmail.com>
2025-06-18 14:50:54 +0200

    random

--- src/app/scryfall/random/random.component.ts
+++ src/app/scryfall/random/random.component.ts
@@ -45,7 +45,7 @@ export class ScryfallRandomComponent {
   }
 
   async getRandom(idioma?: string) {
-    this.service.getCaras(idioma).subscribe({
+    this.service.getRandom(idioma).subscribe({
       next: (carta) => {
         this.carta.set(carta)
         if (typeof carta.card_faces !== 'undefined') {
